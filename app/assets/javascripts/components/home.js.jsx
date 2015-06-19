813edfb1297a5170f7147576e2670e45f738792e
amatherne <pnkdrmmr@gmail.com>
2015-06-19 15:45:05 -0500

    trial content and some styling to main page

--- app/assets/javascripts/components/home.js.jsx
+++ app/assets/javascripts/components/home.js.jsx
@@ -19,8 +19,17 @@ var Home = React.createClass({
 					</nav>
 				</header>
 				<div className="jumbotron">
+					<h1>Welcome to ATX Floods.</h1>
+					<p>An look into the forgotten flood zones</p>
 				</div>
 				<article>
+					<div className="content">
+						<img src="../images/rbz-2013-favs-03.jpg"/>
+						<h2>headlines baby</h2>
+						<p>High Life YOLO umami, pork belly Thundercats cray disrupt taxidermy art party tilde flannel food truck Brooklyn locavore. Ethical Shoreditch bitters artisan. Ennui organic four loko, Schlitz vinyl food truck butcher scenester before they sold out Pitchfork Brooklyn lo-fi. Kitsch yr banh mi cornhole. VHS fanny pack retro authentic bicycle rights. Echo Park mustache Austin brunch. Roof party food truck ennui, shabby chic pop-up sustainable pickled umami 8-bit aesthetic lumbersexual butcher.</p>
+						<br>
+						<p>Asymmetrical kitsch biodiesel, lo-fi disrupt viral DIY Carles blog bicycle rights sriracha next level tousled cold-pressed food truck. Crucifix raw denim drinking vinegar, twee Pitchfork pickled cliche sriracha ennui Brooklyn church-key. Next level skateboard fingerstache, Banksy Austin quinoa health goth deep v. Keytar Pinterest Shoreditch, bespoke actually semiotics fingerstache iPhone health goth gentrify High Life pour-over small batch tilde. Beard four dollar toast tattooed Thundercats. Fingerstache kogi pour-over, polaroid ennui chambray iPhone mixtape selfies photo booth wayfarers mumblecore cronut fap hella. Pinterest dreamcatcher fixie beard, heirloom tote bag ethical fanny pack occupy fingerstache authentic sustainable roof party squid polaroid.</p>
+					</div>
 				</article>
 			</section>
 		);
