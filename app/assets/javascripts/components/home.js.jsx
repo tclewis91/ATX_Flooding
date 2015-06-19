df2fd39adda6bbc42305fe6c14914f9ef44b1d00
amatherne <pnkdrmmr@gmail.com>
2015-06-19 14:44:38 -0500

    beginning of content and styling for main page

--- app/assets/javascripts/components/home.js.jsx
+++ app/assets/javascripts/components/home.js.jsx
@@ -2,7 +2,26 @@ var Home = React.createClass({
 	render: function () {
 		return (
 			<section>
-				
+				<header>
+					<nav>
+						<div className="left">
+							<img src=""/>
+						</div>
+						<div className="right">
+							<ul>
+								<li><a href="">Home</a></li>
+								<li><a href="">Login</a></li>
+								<li><a href="">Forums</a></li>
+								<li><a href="">Search</a></li>
+								<li><a href="">Profile</a></li>
+							</ul>
+						</div>
+					</nav>
+				</header>
+				<div className="jumbotron">
+				</div>
+				<article>
+				</article>
 			</section>
 		);
 	}
