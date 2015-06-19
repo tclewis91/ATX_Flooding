2f7ba1e1fcbd02fa1bb5de987ebcebf685a536ce
Nathaniel Felps <ndfelps@gmail.com>
2015-06-19 17:59:32 -0500

    .

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -12,15 +12,18 @@ var App = Backbone.Router.extend({
         'forums': 'forums',
         'login': 'login',
     },
-        profile: function() {
-            React.render(<Profile/>, document.querySelector('#container'));
+    profile: function() {
+        React.render(<Profile/>, document.querySelector('#container'));
     },
-        home: function() {
-            React.render(<Home/>, document.querySelector('#container'));
+    home: function() {
+        React.render(<Home/>, document.querySelector('#container'));
     },
-        forums: function() {
-            React.render(<Forum/>, document.querySelector('#container'));
+    forums: function() {
+        React.render(<Forum/>, document.querySelector('#container'));
     },
+    login: function() {
+        React.render(<Login/>, document.querySelector('#container'));
+    }
 });
 
 var app = new App();
@@ -48,17 +51,107 @@ function logIn (e) {
 };
 
 function logInPush (e) {
+    var username = $('.userBox').val();
+    var password = $('.passBox').val();
     if(event.keycode === 13) {
 
     }
 };
 
 function signIn (e) {
+    var firstName = $('.firstName').val();
+    var lastName = $('.lastName').val();
+    var email = $('.email').val();
+    var password = $('.password').val();
+    var username = $('.username').val();
+    if ($name.val() === "") {
+            $('#name-error').html("*Please insert a valid name");
+            console.log('why');
+        } else {
+            $('#name-error').html("");
+        }
+        var atSym = false;
+        var eDotCom = false;
+        var e;
+        if ('@' in $email.val().split()) {
+            atSym = true;
+        }
+        if (atSym === false) {
+            $('#email-error').html("*Please insert a valid email address");
+        } else {
+            $('#email-error').html("");
+        }
+        if ($email.val().substring($email.val().length-4) === ".com") {
+            eDotCom = true;
+        }
+        if (eDotCom === false) {
+            $('#email-error').html("*Please insert a valid email address");
+        } else {
+            $('#email-error').html("");
+        }
+        var httpPresent = false;
+        var dotCom = false;
 
+        if ($website.val().substring($website.val().length-4) === ".com") {
+            dotCom = true;
+        }
+        if (dotCom === false && httpPresent === false) {
+            $('#website-error').html("*Please insert a valid website");
+        } else {
+            $('#website-error').html("");
+        }
+        if ($website.val().substring(0, 7) === "http://") {
+            httpPresent = true;
+        }
+        if ($('#website-error').html() === "" && $('#email-error').html() === "" && $('#name-error').html() === "") {
+            console.log('??');
+            $('.contact-box').hide();
+            $('#success').show();
+        }
 };
 
 function signInPush (e) {
+    var firstName = $('.firstName').val();
+    var lastName = $('.lastName').val();
+    var email = $('.email').val();
+    var password = $('.password').val();
+    var username = $('.username').val();
     if(event.keycode === 13) {
-
+        if (firstName === "") {
+            $('.firstError').show();
+        } else {
+            $('.firstError').hide();
+        }
+        if (lastName === "") {
+            $('.lastError').show();
+        } else {
+            $('.lastError').hide();
+        }
+        if (password === "") {
+            $('.passError').show();
+        } else {
+            $('.passError').hide();
+        }
+        var atSym = false;
+        var eDotCom = false;
+        var e;
+        if ('@' in $email.split()) {
+            atSym = true;
+        }
+        if (atSym === false) {
+            $('.emailError').show();
+        } else {
+            $('.emailError').hide();
+        }
+        if (email.substring(email.length-4) === ".com") {
+            eDotCom = true;
+        }
+        if (eDotCom === false) {
+            $('.emailError').show();
+        } else {
+            $('.emailError').hide();
+        }
+        var httpPresent = false;
+        var dotCom = false;
     }
 };
