7ce95955f0c1da55f001f6f9ebc00132d7c4cdcb
tclewis91 <tclewis91@gmail.com>
2015-06-19 19:42:10 -0500

    user comma taken out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable
 end
