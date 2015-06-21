5389060fa37f8cb78409c1f952edf5629b48616e
amatherne <pnkdrmmr@gmail.com>
2015-06-21 12:00:24 -0500

    login page styles finished

--- app/assets/javascripts/components/login.js.jsx
+++ app/assets/javascripts/components/login.js.jsx
@@ -4,24 +4,28 @@ var Login = React.createClass({
 			<section className="main">
 				<div className="login-tron">
 					<div className="logo">
-						<h1>Sign up below.</h1>
+						<h1>Welcome.</h1>
 					</div>
 				</div>
-				<form>
-					<h2>Log in...</h2>
-					<input type = "text" className = "userBox" placeholder = "Username" /> <span className = "error userError">*Your username or password is incorrect</span>
-					<input type = "password" className = "passBox" placeholder = "Password" />
-					<button type = "submit" className = "logButton">Submit</button>
-				</form>
-				<form>
-					<h2>Or sign up!</h2>
-					<input type = "text" className = "firstName" placeholder = "First Name" /> <span className = "error firstError">*you must ender a valid name</span>
-					<input type = "text" className = "lastName" placeholder = "Last Name" /> <span className = "error lastError">*you must ender a valid name</span>
-					<input type = "text" className = "username" placeholder = "Username" /> <span className = "error signError">*you must ender a valid username</span>
-					<input type = "text" className = "email" placeholder = "Email" /> <span className = "error emailError">*you must ender a valid email</span>
-					<input type = "password" className = "password" placeholder = "Password" /> <span className = "error passError">*you must ender a valid password</span>
-					<button className = "signButton">Submit</button>
-				</form>
+				<div className="login-input">
+					<form>
+						<h2>Log in...</h2>
+						<input type = "text" className = "userBox" placeholder = "Username" /> <span className = "error userError">*Your username or password is incorrect</span>
+						<input type = "password" className = "passBox" placeholder = "Password" />
+						<button type = "submit" className = "logButton">Submit</button>
+					</form>
+				</div>
+				<div className="login-signup">
+					<form>
+						<h2>Or Sign Up!</h2>
+						<input type = "text" className = "firstName" placeholder = "First Name" /> <span className = "error firstError">*you must ender a valid name</span>
+						<input type = "text" className = "lastName" placeholder = "Last Name" /> <span className = "error lastError">*you must ender a valid name</span>
+						<input type = "text" className = "username" placeholder = "Username" /> <span className = "error signError">*you must ender a valid username</span>
+						<input type = "text" className = "email" placeholder = "Email" /> <span className = "error emailError">*you must ender a valid email</span>
+						<input type = "password" className = "password" placeholder = "Password" /> <span className = "error passError">*you must ender a valid password</span>
+						<button className = "signButton">Submit</button>
+					</form>
+				</div>
 			</section>
 		);
 	}
