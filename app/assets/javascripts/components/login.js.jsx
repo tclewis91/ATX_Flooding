963d083353ddeab69bcb22a61ea5532799d60237
Nathaniel Felps <ndfelps@gmail.com>
2015-06-21 10:51:15 -0500

    .

--- app/assets/javascripts/components/login.js.jsx
+++ app/assets/javascripts/components/login.js.jsx
@@ -10,12 +10,12 @@ var Login = React.createClass({
 				</form>
 				<form>
 					<h2>Or sign up!</h2>
-					<input type = "text" className = "firstName" placholder = "First Name" /> <span className = "error firstError">*you must ender a valid name</span>
-					<input type = "text" className = "lastName" placholder = "Last Name" /> <span className = "error lastError">*you must ender a valid name</span>
-					<input type = "text" className = "username" placholder = "Username" /> <span className = "error signUserError">*you must ender a valid username</span>
-					<input type = "text" className = "email" placholder = "Email" /> <span className = "error emailError">*you must ender a valid email</span>
-					<input type = "password" className = "password" placholder = "Password" /> <span className = "error passError">*you must ender a valid password</span>
-					<button type = 'submit' className = "signButton">Submit</button>
+					<input type = "text" className = "firstName" placeholder = "First Name" /> <span className = "error firstError">*you must ender a valid name</span>
+					<input type = "text" className = "lastName" placeholder = "Last Name" /> <span className = "error lastError">*you must ender a valid name</span>
+					<input type = "text" className = "username" placeholder = "Username" /> <span className = "error signError">*you must ender a valid username</span>
+					<input type = "text" className = "email" placeholder = "Email" /> <span className = "error emailError">*you must ender a valid email</span>
+					<input type = "password" className = "password" placeholder = "Password" /> <span className = "error passError">*you must ender a valid password</span>
+					<button className = "signButton">Submit</button>
 				</form>
 			</section>
 		);
