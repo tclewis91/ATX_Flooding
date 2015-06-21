7708352cd7fee97c41dd3929787b3196cec01d3b
amatherne <pnkdrmmr@gmail.com>
2015-06-21 14:42:27 -0500

    had to redo the styles to forum page

--- app/assets/javascripts/components/forum.js.jsx
+++ app/assets/javascripts/components/forum.js.jsx
@@ -9,23 +9,23 @@ var Forum = React.createClass({
             </div>
             <div className="forum-select">
             	<ul className="forum-button">
-	            	<li className="button"><a href = "#1">Road Closures</a></li>
+	            	<li className="button left"><a href = "#1">Road Closures</a></li>
 					<li className="button"><a href = "#2">Volunteer Work</a></li>
 					<li className="button"><a href = "#3">Shelter Information</a></li>
-					<li className="button"><a href = "#4">General Board</a></li>
+					<li className="button right"><a href = "#4">General Board</a></li>
 				</ul>
 			</div>
 			<div className="forumBox">
 				<section id = "chatArea1" className = 'chats page'>
 					<div className="box chatBox" ref = "1"></div>
 				</section>
-				<section id = "chatArea2" className = ' chats page'>
+				<section id = "chatArea2" className = 'page'>
 					<div className="box chatBox" ref = "2"></div>
 				</section>
-				<section id = "chatArea3" className = 'chats page'>
+				<section id = "chatArea3" className = 'page'>
 					<div className="box chatBox" ref = "3"></div>
 				</section>
-				<section id = "chatArea4" className = 'chats page'>
+				<section id = "chatArea4" className = 'page'>
 					<div className="box chatBox" ref = "4"></div>
 				</section>
 			</div>
