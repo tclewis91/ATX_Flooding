fb5d483fcfcb3528aed9bdcc3a8867dba5ba1bb3
tclewis91 <tclewis91@gmail.com>
2015-06-21 18:14:20 -0500

    making devise json friendly

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
 
   resources :forum_threads do
   resources :forum_posts
   end
 
-  root to: 'application#index'
+  root to: 'application#login'
 end
